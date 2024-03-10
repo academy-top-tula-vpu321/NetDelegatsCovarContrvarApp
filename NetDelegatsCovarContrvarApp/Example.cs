51144720eff00c048fa343b6f831aed5876cd137
maxefimov <efimov.top.academy@gmail.com>
2024-03-10 10:16:07 +0300

    Добавьте файлы проекта.

---
+++ NetDelegatsCovarContrvarApp/Example.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetDelegatsCovarContrvarApp
+{
+    internal static class Example
+    {
+        static public void Covariant()
+        {
+            // Covarint
+            MessageBuilder messageBuilder = EmailMessageCreate;
+
+            Message message = messageBuilder("Hello world");
+            message.Print();
+
+
+            // Contrvariant
+            EmailReceiver emailReceiver = ReceiveMessage;
+            emailReceiver(new EmailMessage("Hello from email"));
+
+
+            void ReceiveMessage(Message message) => message.Print();
+
+            EmailMessage EmailMessageCreate(string text) => new EmailMessage(text);
+        }
+    }
+
+    class Message
+    {
+        public string? Text { get; set; }
+        public Message(string? text) => Text = text;
+        public virtual void Print() => Console.WriteLine($"Message: {Text}");
+    }
+
+    class EmailMessage : Message
+    {
+        public EmailMessage(string? text) : base(text) { }
+        public override void Print()
+        {
+            Console.WriteLine($"Email Message: {Text}");
+        }
+    }
+
+    class SmsMessage : Message
+    {
+        public SmsMessage(string? text) : base(text) { }
+        public override void Print()
+        {
+            Console.WriteLine($"Sms Message: {Text}");
+        }
+    }
+
+
+    // covariant
+    delegate Message MessageBuilder(string? text);
+
+    // contrvariant
+    delegate void EmailReceiver(EmailMessage message);
+}
---
+++ NetDelegatsCovarContrvarApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using NetDelegatsCovarContrvarApp;
+
+//Example.Covariant();
+
+// covariant
+MessageBuilder<EmailMessage> EmailMessageCreateHandler = EmailMessageCreate;
+MessageBuilder<Message> MessageCreateHandler = EmailMessageCreate;
+
+EmailMessage EmailMessageCreate(string text)
+{
+    return new EmailMessage(text);
+}
+
+// contrvariant
+MessageReceiver<Message> messageReceiver = MessagePrint;
+
+messageReceiver(new Message("Simple message"));
+messageReceiver(new EmailMessage("Email message"));
+
+void MessagePrint(Message message) => message.Print();
+//{
+//    message.Print();
+//}
+
+
+// contrvarian and covariant
+MessageConverter<Message, EmailMessage> toEmailCoverter = MessageToEmailMessage;
+MessageConverter<SmsMessage, Message> toMessageCoverter = toEmailCoverter;
+
+
+EmailMessage emessage = toEmailCoverter(new Message("Message to Email object"));
+emessage.Print();
+
+var message = toMessageCoverter(new SmsMessage("Sms to Message object"));
+message.Print();
+
+EmailMessage MessageToEmailMessage(Message message) => new EmailMessage(message.Text);
+
+
+delegate T MessageBuilder<out T>(string text);
+
+delegate void MessageReceiver<in T>(T message);
+
+delegate T2 MessageConverter<in T1, out T2>(T1 Message);
+
